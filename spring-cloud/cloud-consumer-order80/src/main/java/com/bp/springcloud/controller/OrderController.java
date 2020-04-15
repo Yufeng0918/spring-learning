672f67ed6acfc64a902a6b9a406c708836cc8299
daiyu <jiangyufeng.0918@gmail.com>
2020-04-15 23:27:07 +0800

    springboot consul

--- spring-cloud/cloud-consumer-order80/src/main/java/com/bp/springcloud/controller/OrderController.java
+++ spring-cloud/cloud-consumer-order80/src/main/java/com/bp/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.bp.springcloud.controller;
 import com.bp.springcloud.entities.CommonResult;
 import com.bp.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,12 +27,26 @@ public class OrderController {
 
     @GetMapping("/consumer/payment/create")
     public CommonResult<Payment> create(Payment payment) {
+
         return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
     }
 
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
+
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPaymentEntity(@PathVariable("id") Long id) {
+
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+
+        if (entity.getStatusCode().is2xxSuccessful()) {
+            return entity.getBody();
+        }
+
+        return new CommonResult<>(444, "failure");
     }
 
 }
