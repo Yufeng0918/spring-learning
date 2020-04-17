ba62e455d1d02496074ea00a464829728d82fe3b
daiyu <jiangyufeng.0918@gmail.com>
2020-04-17 09:25:29 +0800

    move spring-boot and spring-cloud

--- spring-boot/spring-boot-02-config/src/main/java/com/bp/springboot/service/HelloService.java
+++
@@ -1,4 +0,0 @@
-package com.bp.springboot.service;
-
-public class HelloService {
-}
--- spring-boot/spring-boot-09-jpa/src/main/java/com/bp/springboot/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bp.springboot.repository;
-
-import com.bp.springboot.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-}
